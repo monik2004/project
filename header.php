cd63a1bc3401468dfa68feaad41929f1f813411a
monik2004 <monikrupareliya123@gmail.com>
2023-08-14 23:11:03 +0530

    change in nav
    In this code nav bar using div tag

--- header.php
+++ header.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
-    <header>
+<nav>
+    <div class="menu">
         <div class="logo">
-            <img src="img/heart1.png" alt="">
+            <img src="img/heart1.png">
         </div>
-        <ul>
-            <li> <a href="index.php"> Home </a> </li>
-            <li> <a href="aboutUs.php"> About Us </a> </li>
-            <li> <a href="contectUs.php"> Contect Us </a> </li>
-            <li> <a href="#"> Register  </a> </li>
-            <li> <a href="#"> Login </a> </li>
-        </ul>
-    </header>
+        <div class="menuItem"><a href="index.php"> Home </a></div>
+        <div class="menuItem"><a href="aboutUs.php"> About Us </a></div>
+        <div class="menuItem"><a href="contectUs.php"> Contect us </a></div>
+        <div class="menuItem"><a> Register </a>
+            <div class="subMenu">
+                <div class="subMenuItem"> <a href="#"> Register Doner/Reciver </a> </div>
+                <div class="subMenuItem"> <a href="#"> Register Hospital </a> </div>
+                <div class="subMenuItem"> <a href="#"> Register Blood Bank </a> </div>
+            </div>
+        </div>
+        <div class="menuItem"><a href="#"> Log In </a></div>
+    </div>
+    </nav>
 </body>
+
 </html>
